a4a3ad71905f766aeb78d8f17a6ed7c34768c4c6
cyberknight42 <xiejw@shanghaitech.edu.cn>
2022-09-28 15:32:46 +0800

    update

--- code/bev_toolbox/init_toolbox.py
+++ code/bev_toolbox/init_toolbox.py
@@ -5,4 +5,4 @@ def init_toolbox_mmdet3d():
     '''
     Init wrappers for mmdet3d.
     '''
-    importlib.import_module('toolbox.wrapper_mmdet3d')
+    importlib.import_module('bev_toolbox.wrapper_mmdet3d')
